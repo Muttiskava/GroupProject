39929b9495833b7ebc7c53d81e2d35410512ba8e
Ahmad <your.email@example.com>
2023-07-13 09:10:41 +0200

    Lägger till kommentarer i koden

--- my-app/src/Components/steg1.js
+++ my-app/src/Components/steg1.js
@@ -1,20 +1,19 @@
 import React from 'react';
-
-import './App.css'
-
-
+import './App.css';
 
 class BookingSystem extends React.Component {
   constructor(props) {
     super(props);
+    // State-variabler som används i komponenten
     this.state = {
-      daysOfWeek: ['Mån', 'Tis', 'Ons', 'Tor', 'Fre', 'Lör', 'Sön'],
-      timeslots: ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'],
-      bookings: {},
-      currentDate: new Date(),
+      daysOfWeek: ['Mån', 'Tis', 'Ons', 'Tor', 'Fre', 'Lör', 'Sön'], // Array med veckodagar
+      timeslots: ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'], // Array med tidsluckor
+      bookings: {}, // Objekt som håller reda på bokningar
+      currentDate: new Date(), // Aktuellt datum
     };
   }
 
+  // Funktion som anropas när en bokning klickas på
   handleBookingClick = (day, time) => {
     const bookings = { ...this.state.bookings };
     if (!bookings[day]) {
@@ -24,6 +23,7 @@ class BookingSystem extends React.Component {
     this.setState({ bookings });
   };
 
+  // Funktion för att gå till föregående vecka
   handlePrevWeek = () => {
     const { currentDate } = this.state;
     const prevWeekDate = new Date(currentDate);
@@ -31,6 +31,7 @@ class BookingSystem extends React.Component {
     this.setState({ currentDate: prevWeekDate });
   };
 
+  // Funktion för att gå till nästa vecka
   handleNextWeek = () => {
     const { currentDate } = this.state;
     const nextWeekDate = new Date(currentDate);
@@ -40,24 +41,23 @@ class BookingSystem extends React.Component {
 
   render() {
     const { daysOfWeek, timeslots, bookings, currentDate } = this.state;
-  
+
     const weekStart = new Date(currentDate);
     weekStart.setDate(currentDate.getDate() + 1 - currentDate.getDay()); // Sätter veckans startdag till måndag
-  
+
     const weekEnd = new Date(weekStart);
     weekEnd.setDate(weekEnd.getDate() + 6); // Sätter veckans slutdag till söndag
-  
+
     return (
       <div>
         <h1 className='header'>Booking System</h1>
         <div className="nextWeek">
           <button onClick={this.handlePrevWeek}>Previous Week</button>
-          <button  onClick={this.handleNextWeek}>Next Week</button>
+          <button onClick={this.handleNextWeek}>Next Week</button>
         </div>
         <table>
           <thead>
             <tr>
-              <th>Tid</th>
               {daysOfWeek.map((day) => (
                 <th key={day}>{day}</th>
               ))}
@@ -65,20 +65,18 @@ class BookingSystem extends React.Component {
           </thead>
           <tbody>
             <tr>
-              <td>Date</td>
               {daysOfWeek.map((day, index) => {
                 const date = new Date(weekStart);
                 date.setDate(date.getDate() + index);
                 return (
-                  <td key={index}>
-                    {date.getDate()} {/* Visa endast dagen */}
+                  <td key={index} className="date-cell">
+                    {date.getDate()}
                   </td>
                 );
               })}
             </tr>
             {timeslots.map((time) => (
               <tr key={time}>
-                <td>{time}</td>
                 {daysOfWeek.map((day, index) => {
                   const date = new Date(weekStart);
                   date.setDate(date.getDate() + index);
@@ -99,7 +97,6 @@ class BookingSystem extends React.Component {
       </div>
     );
   }
-  
 }
 
 export default BookingSystem;
